6ac9ca60a04422df82d14859aa89d501aef15a8a
Daniel Mackey <dmackey@bellycard.com>
2014-10-09 16:33:50 -0500

    Pull publish mechanism out of AR callbacks for re-use outside of callbacks

--- lib/napa_rabbit_publisher.rb
+++ lib/napa_rabbit_publisher.rb
@@ -3,4 +3,9 @@ module NapaRabbitPublisher
   require 'napa_rabbit_publisher/version'
   require 'napa_rabbit_publisher/amqp_singleton'
   require 'napa_rabbit_publisher/rabbit_active_record_callbacks'
+
+  def self.publish(data, routing_key)
+    amqp = NapaRabbitPublisher::AMQPSingleton.instance
+    amqp.exchange.publish(data.to_json, routing_key: routing_key, content_type: 'application/json')
+  end
 end
