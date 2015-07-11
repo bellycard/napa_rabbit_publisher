a62bdbf051d18beb2a3fb64d4f38fe3a7ac14964
Darby Frey <darbyfrey@gmail.com>
2015-07-11 12:12:57 -0500

    Adding support for CLOUDAMQP_URL configuration

--- lib/napa_rabbit_publisher/amqp_singleton.rb
+++ lib/napa_rabbit_publisher/amqp_singleton.rb
@@ -5,7 +5,7 @@ module NapaRabbitPublisher
     attr_reader :connection, :channel, :exchange
     # we only want one connection at a time (most of the time) to RabbitMQ
     def initialize
-      @connection = Bunny.new(ENV['AMQP_HOST'])
+      @connection = Bunny.new(ENV['CLOUDAMQP_URL'] || ENV['AMQP_HOST'])
       @connection.start
       @channel = connection.create_channel
       @exchange = channel.topic(ENV['SERVICE_NAME'])
